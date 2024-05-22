6fcb424e5c18115946877a829a8c28dd8082e0b7
kurasitayou <n23012@std.it-college.ac.jp>
2024-05-22 05:09:31 +0000

    修正

--- TextBook/ch06/pa.py
+++
@@ -1,6 +0,0 @@
-def pig_latin(text):
-    return ' '.join((word + 'yay') if word[0].isalpha() and word[0] in 'aeiouAEIOU' else (word[2:] + word[:2] + 'ay') if word[:1].isalpha() and word[:2] not in 'gaeiouAEIOU' else (word[1:] + word[0] + 'ay') if word[0].isalpha() else (word[2:] + word[:2] + 'ay') if word[:1].isalpha() and word[:2] not in 'gaeiouAEIOU'else word for word in text.split())
-
-text = input()
-print(pig_latin(text))
-
--- TextBook/ch06/pa2.py
+++
@@ -1,6 +0,0 @@
-def pig_latin(word): return word[1:] + word[0] + 'ay' if word[0] not in 'aeiou' else word + 'ay'
-
-# Example usage:
-text = "google chat"
-print(pig_latin(text))  # Output: "ooglegay atchay"
-
